636d13a946047e1502a11be63f8f9a0143543ac4
LostMoa <life4458@naver.com>
2022-08-25 16:56:28 +0900

    버튼 몇개 추가
    -

---
+++ Assets/Scripts/OptionButton.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionButton : MonoBehaviour
+{
+    public static OptionButton instance;
+    public GameObject optionGrayWindow;
+
+    private void Awake()
+    {
+        if(instance!=null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+        DontDestroyOnLoad(this.gameObject);
+    }
+    public void SelectOptionButtonOpen()
+    {
+        optionGrayWindow.SetActive(true);
+    }
+
+    public void SelectOptionButtonClose()
+    {
+        optionGrayWindow.SetActive(false);
+    }
+}

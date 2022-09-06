0194855723e7ff682299b8ee3ce689f09397a462
LostMoa <life4458@naver.com>
2022-09-06 17:39:29 +0900

    버튼Child 스케일러 추가

---
+++ Assets/Scripts/AbilityButtonChildScaler.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AbilityButtonChildScaler : MonoBehaviour
+{
+    [SerializeField] GameObject abilityImage;
+    [SerializeField] GameObject abilityInfoText;
+}

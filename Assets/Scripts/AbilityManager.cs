407cf932ef2deb0a8c905958de8d2cb5c8b2c76b
LostMoa <life4458@naver.com>
2022-09-16 14:34:03 +0900

    abilities 이름 변경

--- Assets/Scripts/AbilityManager.cs
+++ Assets/Scripts/AbilityManager.cs
@@ -2,13 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Abilities : MonoBehaviour
+public class AbilityManager : MonoBehaviour
 {
     public static Ability[] abilities;
     public static int[] maxAbilityLevel;
     public static int[] nowAbilityLevel;
     public enum AbilityName_
     {
-
+        BubbleMissile,
     }
 }

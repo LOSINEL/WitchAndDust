97c64bef6fbdcc5b87640d05c276f1d03a55a614
LostMoa <life4458@naver.com>
2022-10-11 12:41:39 +0900

    20221011 update

--- Assets/Scripts/Ability.cs
+++ Assets/Scripts/Ability.cs
@@ -1,11 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 [CreateAssetMenu(fileName = "New Ability", menuName = "Ability")]
 public class Ability : ScriptableObject
 {
-    public Image abilityImage;
+    public Sprite abilityImage;
     public string[] abilityInfo;
     public string abilityName;
     public GameObject abilityPrefab;
